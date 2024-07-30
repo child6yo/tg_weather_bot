d34fc3c33aed8a25732bd8e52f99cc16f7cccefc
Arseniy Glushkov <miguelprod8@gmail.com>
2024-07-31 00:42:17 +0700

    /weather comand added

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,5 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart
+from aiogram.filters import Command
 from aiogram.types import Message
 from lexicon.lexicon_ru import LEXICON_RU
 from services.main_service import get_answer
@@ -25,10 +26,21 @@ async def cmd_start(message: Message):
         await message.answer(text=LEXICON_RU["НОВЫЙ ПОЛЬЗОВАТЕЛЬ"])
     else:
         await message.answer(text=LEXICON_RU["СТАРЫЙ ПОЛЬЗОВАТЕЛЬ"])
+    
+@router.message(Command(commands="weather"))
+async def show_weather(message: Message):
+    user_info = await db_manager.get_user_data(user_id=message.from_user.id)
 
+    if user_info["city"]:
+        try:
+            await message.answer(text=get_answer(user_info["city"]))
+        except:
+            await message.answer(text=LEXICON_RU["ОШИБКА"])
+    else:
+        await message.answer(text=LEXICON_RU["ОШИБКА ГОРОДА"])
 
 @router.message()
-async def send_current_weather(message: Message):
+async def change_user_city_and_show_weather(message: Message):
     try:
         await message.answer(text=get_answer(message.text))
         await db_manager.change_user_city(
