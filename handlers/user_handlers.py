e9cc67c83f393e45b4f0d7180628288685ab61d9
Arseniy Glushkov <miguelprod8@gmail.com>
2024-07-31 00:30:38 +0700

    Some changes

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -3,25 +3,36 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from lexicon.lexicon_ru import LEXICON_RU
 from services.main_service import get_answer
-from transliterate import translit
-
-import urllib.error
+from database import db_manager
 
 router = Router()
 
 
-# Этот хэндлер срабатывает на команду /start
+# хэндлер на команду /start
 @router.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer(text=LEXICON_RU['/start'])
+async def cmd_start(message: Message):
+    user_info = await db_manager.get_user_data(user_id=message.from_user.id)
 
-# Этот хэндлер срабатывает на все остальные сообщения и обращается к погодному сервису
+    if len(user_info) == 0:
+        await db_manager.insert_user(
+            user_data={
+                "user_id": message.from_user.id,
+                "full_name": message.from_user.full_name,
+                "user_login": message.from_user.username,
+                "city": None,
+            }
+        )
+        await message.answer(text=LEXICON_RU["НОВЫЙ ПОЛЬЗОВАТЕЛЬ"])
+    else:
+        await message.answer(text=LEXICON_RU["СТАРЫЙ ПОЛЬЗОВАТЕЛЬ"])
+
+
 @router.message()
 async def send_current_weather(message: Message):
-    latin_message = translit(message.text, language_code='ru', reversed=True)
     try:
-       await message.answer(text=get_answer(latin_message))
-    except urllib.error.HTTPError:
-       await message.answer(text=LEXICON_RU['ОШИБКА НАЗВАНИЯ'])
-    except UnicodeEncodeError:
-        await message.answer(text=LEXICON_RU['ОШИБКА ЯЗЫКА'])
+        await message.answer(text=get_answer(message.text))
+        await db_manager.change_user_city(
+            user_id=message.from_user.id, city=message.text
+        )
+    except:
+        await message.answer(text=LEXICON_RU["ОШИБКА"])
--- services/weather/weather_api_service.py
+++ services/weather/weather_api_service.py
@@ -1,8 +1,5 @@
 from typing import NamedTuple
-from services.weather.config import Config, load_config
-import urllib.request, json
-
-config: Config = load_config()
+import requests, json
 
 Celsius = int
 
@@ -13,19 +10,62 @@ class Weather(NamedTuple):
     city: str
 
 
+class Coordinates(NamedTuple):
+    latitude: str
+    longtitude: str
+
+
+weather_codes = {0: "Ясно",
+                 1: "В основном ясно",
+                 2: "Частично облачно",
+                 3: "Облачно",
+                 45: "Туман",
+                 48: "Холодный туман",
+                 51: "Морось",
+                 53: "Мелкий дождь",
+                 55: "Плотная морось",
+                 56: "Морозящая морось",
+                 57: "Морозящая морось",
+                 61: "Небольшой дождь",
+                 63: "Умеренный дождь",
+                 65: "Сильный дождь",
+                 66: "Холодный дождь",
+                 67: "Сильный холодный дождь",
+                 71: "Слабый снегопад",
+                 73: "Средний снегопад",
+                 75: "Сильный снегопад",
+                 77: "Снежные крупинки",
+                 80: "Легкий ливень",
+                 81: "Средний ливень",
+                 82: "Сильный ливень",
+                 85: "Небольшая снежная буря",
+                 86: "Снежная буря"}
+
+
+def get_city_coordinates(city_name: str) -> Coordinates:
+    data = requests.get(
+        f"https://geocoding-api.open-meteo.com/v1/search?name={city_name}&count=1&language=ru&format=json"
+    ).json()
+    print(data)
+    print(city_name)
+    lat = data["results"][0]["latitude"]
+    lon = data["results"][0]["longitude"]
+    return Coordinates(latitude=lat, longtitude=lon)
+
+
 def get_weather(city_name: str) -> Weather:
-    weather_data = weather_parcer(city_name)
-    temp = int(weather_data["main"]["temp"])
-    w_type = str(weather_data["weather"][0]["description"])
-    place = str(weather_data["name"])
-    return Weather(temperature=temp, weather_type=w_type, city=place)
+    coordinates = get_city_coordinates(city_name)
+    weather_data = weather_parcer(coordinates)
+    tempetature = int(weather_data["current"]["temperature_2m"])
+    w_type = weather_codes[int(weather_data["current"]["weather_code"])]
+    city_name = city_name
+    return Weather(temperature=tempetature, weather_type=w_type, city=city_name)
 
 
-def weather_parcer(city_name: str) -> dict:
-    openweather_api = config.weather_service.api_key
-    openweather_url = config.weather_service.url.format(
-        city=city_name, api_key=openweather_api
-    )
-    with urllib.request.urlopen(openweather_url) as url:
-        weather_data = json.load(url)
+def weather_parcer(coordinates: Coordinates) -> dict:
+    lat = coordinates.latitude
+    lon = coordinates.longtitude
+    weather_data = requests.get(
+        f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,weather_code"
+    ).json()
     return weather_data
